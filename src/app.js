cf29e04c429afc468133840716d5790e457113b7
Yogesh Basavaraju <w1853885@westminster.ac.uk>
2025-03-05 13:54:19 +0530

    route changed

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
-app.use("/user", router);
+app.use("/", router);
 
 app.listen(process.env.PORT, () => {
   connectDB()
